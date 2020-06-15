4f18a2b31343e1717e92dbc6ded54356c0967627
ILoveStudying <754070493@qq.com>
2020-06-15 22:50:35 +0800

    option parameters

--- opts.py
+++ opts.py
@@ -7,20 +7,26 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self):
+        # filename
         self.parser.add_argument('--dataset', type=str, default='./dataset/pems08.npz', help='path for dataset')
         self.parser.add_argument('--save_path', type=str, default='./checkpoint/PEMS08', help='path for saving model')
         self.parser.add_argument('--adj', type=str, default='./dataset/distance08.csv', help='filename for adjacency matrix')
         self.parser.add_argument('--Multidataset', type=str, default='./dataset/pems08_h2_d1_w1_p12_s1_MultiComponent.npz', help='whether existing Multidataset or Slidedataset, create one if not')
 
+        # 
         self.parser.add_argument('--process_method', type=str, default='MultiComponent', help='MultiComponent |SlideWindow')
-        self.parser.add_argument('--hdwps', type=str, default='2,1,1,12,1', help='hour, day, week, and shift are multiples of prediction length, ')
+        self.parser.add_argument('--hdwps', type=str, default='2,1,1,12,1', help='hour, day, week, and shift are multiples of prediction length ')
 
+        # GCN-based model network parameter
         self.parser.add_argument('--gcn1_out_feature', type=int, default=128, help='out_feature of GCN layer1')
         self.parser.add_argument('--gcn2_out_feature', type=int, default=64, help='out_feature of GCN layer2')
         self.parser.add_argument('--nb_time_filter', type=int, default=64, help='out_feature of CNN')
-        self.parser.add_argument('--lstm_hidden', type=int, default=32, help='hidden size of lstm or gru')
         self.parser.add_argument('--dropout', type=float, default=0.8, help='only for DM-RGCN')
         self.parser.add_argument('--weight_decay', type=float, default=5e-4, help='weight decay')
+
+        # hidden_size for lstm and gru
+        self.parser.add_argument('--lstm_hidden', type=int, default=32, help='hidden size of lstm or gru')
+
         self.parser.add_argument('--model', type=str, default='DMRGCN', help='DMRGCN |Baseline_LSTM |Baseline_GRU |MCSTGCN |ASTGCN |DM_LSTM_GCN')
 
     def parse(self):
@@ -41,7 +47,7 @@ class TrainOptions(BaseOptions):
         BaseOptions.initialize(self)
         self.parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
         self.parser.add_argument('--optimizer', type=str, default='adam', help='[sgd | adam]')
-        self.parser.add_argument('--epoch', type=int, default=4, help='number of training epochs')
+        self.parser.add_argument('--epoch', type=int, default=150, help='number of training epochs')
         self.isTrain = True
 
 
