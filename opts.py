c385c92e2cb2789cc4acf53a6825b59d14297d2c
ILoveStudying <754070493@qq.com>
2020-06-18 17:12:43 +0800

    Parameters setting

--- opts.py
+++ opts.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import argparse
 
+
 class BaseOptions():
     def __init__(self):
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -11,14 +12,16 @@ class BaseOptions():
         self.parser.add_argument('--dataset', type=str, default='./dataset/pems08.npz', help='path for dataset')
         self.parser.add_argument('--save_path', type=str, default='./checkpoint/PEMS08', help='path for saving model')
         self.parser.add_argument('--adj', type=str, default='./dataset/distance08.csv', help='filename for adjacency matrix')
-        self.parser.add_argument('--Multidataset', type=str, default='./dataset/pems08_h2_d1_w1_p12_s1_MultiComponent.npz', help='whether existing Multidataset or Slidedataset, create one if not')
+        self.parser.add_argument('--Multidataset', type=str, default='./dataset/pems08_h2_d1_w1_p12_s1_MultiComponent.npz',
+                                 help='whether existing Multidataset or Slidedataset, create one if not')
 
         '''
-        if '--Multidataset' is not existing, create one based on '--hdwps'. Note that the hour, day, week, prediction, 
-        and shift in '--Multidataset' should be consistent with the ones in '--hdwps'.  
-        eg. 'h2_d1_w1_p12_s1' in '--Multidataset' and '2,1,1,12,1' in '--hdwps'
+        if '--Multidataset' is not existing, create one based on '--hdwps'.
+        Note that we use the same Multidataset as p=12(1 hour) when predicting p = 6(30min) or 3(15min) in '--hdwps'
+        e.g We predict different time slices p all based on pems08_h2_d1_w1_p12_s1_MultiComponent.npz  
         '''
-        self.parser.add_argument('--hdwps', type=str, default='2,1,1,12,1', help='hour, day, week, and shift are multiples of prediction length ')
+        self.parser.add_argument('--hdwps', type=str, default='2,1,1,12,1',
+                                 help='hour(h), day(d), week(w), and shift(s) are multiples of prediction(p) ')
         self.parser.add_argument('--process_method', type=str, default='MultiComponent', help='MultiComponent |SlideWindow')
 
         # GCN-based model network parameter
@@ -31,7 +34,8 @@ class BaseOptions():
         # hidden_size for lstm and gru
         self.parser.add_argument('--lstm_hidden', type=int, default=64, help='hidden size of lstm or gru')
 
-        self.parser.add_argument('--model', type=str, default='DMRGCN', help='DMRGCN |Baseline_LSTM |Baseline_GRU |MCSTGCN |ASTGCN |DM_LSTM_GCN')
+        self.parser.add_argument('--model', type=str, default='DMRGCN',
+                                 help='DMRGCN |Baseline_LSTM |Baseline_GRU |MCSTGCN |ASTGCN |DM_LSTM_GCN')
 
     def parse(self):
         if not self.initialized:
